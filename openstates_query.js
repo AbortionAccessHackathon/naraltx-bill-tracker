d7ffd962791a02f70e554a59d80d719d51ab51d6
colinxfleming <c3flemin@gmail.com>
2018-06-23 11:51:08 -0500

    moar data mapping

--- openstates_query.js
+++ openstates_query.js
@@ -3,19 +3,40 @@ const request = require('superagent');
 const openstatesApiKey = '82e9cac0-93e7-496c-9a76-565ed6233fdc';
 const openstatesEndpoint = 'https://openstates.org/api/v1/bills';
 const searchWindow = ''
-const openstatesParams = { apikey: openstatesApiKey, state: 'tx', search_window: 'session', sort: 'updated_at' };
+const openstatesParams = {
+  apikey: openstatesApiKey,
+  state: 'tx',
+  search_window: 'session',
+  sort: 'updated_at',
+  fields: 'bill_id,created_at,updated_at,title,session,action_dates,subjects',
+  subject: 'Reproductive Issues' // TODO URL for this document, last action
+};
 
 const openstatesQuery = () => {
   const a = request.get(openstatesEndpoint)
     .query(openstatesParams)
     .then(function(response) {
-      console.log(response);
+      const shapedBills = response.body.map(bill => shapeBill(bill))
+      console.log(shapedBills);
+      return shapedBills;
     });
-  
 }
 
-function reducer(bill) {
-
-}
+function shapeBill(bill) {
+  return { 
+    title: bill.title,
+    filed_date: bill.created_at,
+    updated_date: bill.updated_at,
+    bill_id: bill.bill_id,
+    session: bill.session,
+    last_status: null,
+    last_updated: bill.updated_at,
+    bill_url: null,
+    subjects: bill.subjects,
+  }
+} 
 
 openstatesQuery();
+
+// 'Reproductive Issues'
+// 'Sexual Orientation and Gender Issues'
