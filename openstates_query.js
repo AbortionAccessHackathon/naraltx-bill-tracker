80506cb7d145a2403490154055366aa1d8a4464d
colinxfleming <c3flemin@gmail.com>
2018-06-23 11:32:24 -0500

    working api calls

--- openstates_query.js
+++ openstates_query.js
@@ -1,7 +1,21 @@
-import 'superagent';
+const request = require('superagent');
 
-const openstatesKey = '82e9cac0-93e7-496c-9a76-565ed6233fdc';
+const openstatesApiKey = '82e9cac0-93e7-496c-9a76-565ed6233fdc';
+const openstatesEndpoint = 'https://openstates.org/api/v1/bills';
+const searchWindow = ''
+const openstatesParams = { apikey: openstatesApiKey, state: 'tx', search_window: 'session', sort: 'updated_at' };
 
 const openstatesQuery = () => {
+  const a = request.get(openstatesEndpoint)
+    .query(openstatesParams)
+    .then(function(response) {
+      console.log(response);
+    });
+  
+}
+
+function reducer(bill) {
 
 }
+
+openstatesQuery();
