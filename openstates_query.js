1c75afbe006bd35ee7363fc393819f0f84f967cf
colinxfleming <c3flemin@gmail.com>
2018-06-23 21:24:53 -0500

    add append to google sheets

--- openstates_query.js
+++ openstates_query.js
@@ -1,4 +1,5 @@
 const request = require('superagent');
+const { google }  = require('googleapis');
 
 const openstatesApiKey = '82e9cac0-93e7-496c-9a76-565ed6233fdc';
 const openstatesEndpoint = 'https://openstates.org/api/v1/bills';
@@ -16,27 +17,27 @@ const openstatesQuery = () => {
   // Query OpenStates API
   const data = request.get(openstatesEndpoint)
     .query(openstatesParams)
-    .then(function(response) {
+    .then((response) => {
       const shapedBills = response.body.map(bill => shapeBill(bill))
-      console.log(shapedBills);
+      // console.log(shapedBills);
       return shapedBills;
     });
+    // ;
   return data;
 }
 
 const shapeBill = (bill) => {
   // Reduce raw data to just what we need
   return {
-    title: bill.title,
-    filed_date: bill.created_at,
-    bill_id: bill.bill_id,
     session: bill.session,
+    bill_id: bill.bill_id,
+    title: bill.title,
+    bill_url: shapeBillUrl(bill),
+    filed_date: bill.created_at,
     last_status: bill.actions[bill.actions.length - 1].action,
     last_updated: bill.updated_at,
-    bill_url: shapeBillUrl(bill),
   }
 }
-
 
 const shapeBillUrl = (bill) => {
   // Safely get URL without crashing, since some bills don't have URLs for some reason?
@@ -46,5 +47,40 @@ const shapeBillUrl = (bill) => {
   return null;
 }
 
-const data = openstatesQuery();
-return data;
+// HERE'S THE SHEETS PART
+
+const appendToSheet = (valueSet) => {
+  // Authorize
+  const privatekey = require("/Users/colin/Downloads/Beater.json");
+  const jwtClient = new google.auth.JWT(
+         privatekey.client_email,
+         null,
+         privatekey.private_key,
+         ['https://www.googleapis.com/auth/spreadsheets']);
+  jwtClient.authorize(function (err, tokens) {
+   if (err) {
+     console.log(err);
+     return;
+   } else {
+     console.log("Successfully connected!");
+   }
+  });
+
+  const sheets = google.sheets('v4');
+  const googleSheetId = '1fO-vfsIkNuUnfFFiKtnp0pydHmsaLTFt1VgSvguDiAs';
+  return sheets.spreadsheets.values.append({
+    spreadsheetId: googleSheetId,
+    range: 'Sheet1',
+    auth: jwtClient,
+    valueInputOption: 'RAW',
+    insertDataOption: 'INSERT_ROWS',
+    resource: {
+      values: valueSet.map(bill => [bill.session, bill.bill_id, bill.title, bill.bill_url, bill.filed_date, bill.last_status, bill.last_updated]),
+    },
+  });
+}
+
+
+openstatesQuery().then((set) => appendToSheet(set));
+
+
