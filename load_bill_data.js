b4b8dac30e7da32581824ff2f28a598cf5f005e2
colinxfleming <c3flemin@gmail.com>
2018-06-24 14:18:26 -0500

    final edits before showtime

--- load_bill_data.js
+++ load_bill_data.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var page = 'https://teambilltracker.wordpress.com/home-page/';
+var page = 'http://prochoicetexas.org/?page_id=3782&preview=true';
+var productionPage = 'http://prochoicetexas.org/bill-tracker-2/';
 var apiKey = 'AIzaSyARsKyuI7u_mrrHf5_VVW2PAXVJgbl9L5U';
 var spreadsheetId = '1fO-vfsIkNuUnfFFiKtnp0pydHmsaLTFt1VgSvguDiAs';
 var range = 'A:I';
@@ -17,24 +18,42 @@ function mapRowToJson(headerRow, row) {
  }
  return shapedRow;
 }
+
+function mapToCard(bill) {
+  // console.log(bill);
+  return `<div class="card ${bill['Urgent'] ? 'urgent' : ''}">
+            <div class="row text-center top">
+              ${bill['Bill ID']}
+            </div>
+            <div class="row middle text-center">
+              <div class="col-sm-12">Description: ${bill['Title'].substring(1,100)}...</div>
+            </div>
+            <div class="row bottom">
+              <div class="col-sm-12">
+                  ${bill['Last Status']} at ${bill['Last Updated']}
+              </div>
+            </div>
+          </div>`;
+
+}
+
 jQuery(document).ready(function($) {
-  if (window.location.href === page) {
+  if (window.location.href === page || window.location.href === productionPage) {
    var url = callUrl(spreadsheetId, range);
    jQuery.ajax({
      url: url,
      success: function(data) {
        // Turn it all into JSON
-         
        var headerRow = data.values[0];
        var jsonResult = data.values.slice(1).map(function(row) {
          return mapRowToJson(headerRow, row);
        });
 
-       jQuery('.site-content').empty();
+       // Clear out the div and replace it with results of sheet
+       jQuery('.bill-tracker-content').empty();
        jsonResult.forEach(function(bill) {
          // Do something with bill
-         var stringBill = JSON.stringify(bill);
-         jQuery('.site-content').append(stringBill + '<br>');
+         jQuery('.bill-tracker-content').append(mapToCard(bill));
        });
      }
    });
